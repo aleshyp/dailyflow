f9c65e8bfa076f4c6e7431804f17499b5fbcc3c4
Ian Riley <iancriley@gmail.com>
2024-09-12 22:16:10 -0500

    updated text styles in header

--- src/header.tsx
+++ src/header.tsx
@@ -4,13 +4,22 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 bg-gray-300 shadow-sm dark:bg-gray-700">
       <div className="max-w-7xl p-4 flex gap-4 sm:px-6 lg:px-8">
-        <Link to="/" className="[&.active]:font-regular">
+        <Link
+          to="/"
+          className="[&.active]:font-regular text-gray-600 text-lg hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+        >
           Home
         </Link>
-        <Link to="/habits" className="[&.active]:font-regular">
+        <Link
+          to="/habits"
+          className="[&.active]:font-regular text-gray-600 text-lg hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+        >
           Habits
         </Link>
-        <Link to="/profile" className="[&.active]:font-regular">
+        <Link
+          to="/profile"
+          className="[&.active]:font-regular text-gray-600 text-lg hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+        >
           Profile
         </Link>
       </div>
